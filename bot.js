a0a96bc254ac5823e0f51fb337b64ae2b57771e5
dreeeew <dreew.pham@gmail.com>
2018-08-15 03:34:45 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.on("message", message => {
-  // Your commands here
-  if(message.content === "ping"){
-     // the robot answers pong!
-     message.channel.send("Pong!");
-  };
-  if(message.content === "tired"){
-    message.channel.send("Sleep is for the weak.");
-  };
-  if(message.content === "bitch"){
-    message.channel.send("You are the bitch.");
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+client.on('message', message =>{
+  if(message.content === 'ping'){
+     message.channel.send('pong');
   };
 });
 client.login(process.env.BOT_TOKEN);
